b2b6e4587334f856000e1ac348b2145c5fa16006
brunodf12 <58449962+brunodf12@users.noreply.github.com>
2021-06-29 20:50:32 -0300

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from TwitchChannelPointsMiner.classes.entities.Bet import Strategy, BetSettings,
 from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, StreamerSettings
 
 twitch_miner = TwitchChannelPointsMiner(
-    username="anamelo1413",
+    username="anamelo144",
     password="",           # If no password will be provided, the script will ask interactively
     claim_drops_startup=False,                  # If you want to auto claim all drops from Twitch inventory on the startup
     priority=[                                  # Custom priority in this case for example:
